144631087d89d7a52b9b32d33ba1b1cde1235aca
Arnaud Seguin <aseguin@telelogos.com>
2019-11-18 13:39:55 +0100

    nettoyage

---
+++ Telelogos.Reportings/ChartDetail.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Telelogos.Reportings
+{
+   /// <summary>
+   /// Obtient ou définit un détail de graphique
+   /// </summary>
+   public class ChartDetail
+   {
+      // Id de la donnée
+      public string IdData { get; set; }
+
+      // Label de la donnée
+      public string LabelData { get; set; }
+
+      // Version de la donnée
+      public string VersionData { get; set; }
+
+      // Série de la donnée
+      public string SerieData { get; set; }
+
+      // Valeur de la donnée
+      public decimal ValueData { get; set; }
+   }
+}
---
+++ Telelogos.Reportings/ChartType.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace Telelogos.Reportings
+{
+   public enum ChartType
+   {
+      None,
+      Scatter,
+      Line,
+      Bar,
+      Pie,
+      PolarArea,
+      Radar
+   };
+}
